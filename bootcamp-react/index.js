21e8f57c87869cee119dc64acce797cd28425ced
Romvinicius <vinicius.roman@sptech.school>
2023-04-10 14:36:10 -0300

    adicionando 3 aula de react

---
+++ bootcamp-react/index.js
@@ -0,0 +1,32 @@
+var container = document.getElementById("root");
+
+function Participante(props){
+
+    const [numero, setNumero] = React.useState(0);
+
+    function votar(){
+        setNumero(numero + 1);
+    }
+
+    return (
+        
+        <>
+            <h1>{props.nome}</h1>
+            <h2>{numero}</h2>
+            <button onClick={votar}>Votar</button>
+        </>
+    );
+}
+
+function Votacao(){
+    return(
+        <>
+            <Participante nome="Mc"/>
+            <Participante nome="BK"/>
+            <Participante nome="Papais"/>
+        </>
+    )
+}
+
+ReactDOM.createRoot(container).render(<Votacao />);
+
